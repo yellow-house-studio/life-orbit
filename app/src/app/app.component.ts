bb4bebe86e9b143c7a8585b1bf98e43fa6146021
Johanna Hultén <min.mail@johannap.se>
2025-04-19 16:01:56 +0200

    page infra and layout created

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
